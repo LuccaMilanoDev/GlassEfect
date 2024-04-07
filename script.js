160606820d9bf2a76d8488cf60f1923ae4ef6a0f
LuccaMilanoDev <luccamilano2018@gmail.com>
2024-04-07 16:40:42 -0300

    faltando alguns detalhes

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 let name = document.querySelector("#text")
+let color = document.querySelector("#color")
 let fila=[];
 
 function addFila(){
-    fila.push(name.value)
-    console.log(fila)
+    let cliente ={
+        nome:name.value,
+        cor:color.value
+    }
+    fila.push(cliente)
 }
 function removeFila(){
     fila.shift()
-    console.log(fila)
+    showClient()
+}
+
+
+function showClient(){
+    let filaDisplay = fila.map((cliente, index) => {
+        return `<div style="backdrop-filter: blur(4px); ${cliente.cor}; padding: 10px; margin-bottom: 5px;">
+                    <span style="color: white;">${cliente.nome}</span>
+                    <span style="color: white; margin-left: 10px;">Número: ${index + 1}</span>
+                </div>`;
+    }).join('');
+    document.getElementById("cliente").innerHTML = filaDisplay
 }

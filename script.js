882ca421a8c8cf4196388da0ed29daf7cf88b2f8
LuccaMilanoDev <luccamilano2018@gmail.com>
2024-04-07 17:08:46 -0300

    adicionando detalhes

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-let name = document.querySelector("#text")
-let color = document.querySelector("#color")
-let fila=[];
+let name = document.querySelector("#text");
+let color = document.querySelector("#color");
+let fila = [];
 
-function addFila(){
-    let cliente ={
-        nome:name.value,
-        cor:color.value
-    }
-    fila.push(cliente)
+function addFila() {
+  let cliente = {
+    nome: name.value,
+    cor: color.value,
+  };
+  fila.push(cliente);
 }
-function removeFila(){
-    fila.shift()
-    showClient()
+function removeFila() {
+  fila.shift();
+  showClient();
 }
 
-
-function showClient(){
-    let filaDisplay = fila.map((cliente, index) => {
-        return `<div style="backdrop-filter: blur(4px); ${cliente.cor}; padding: 10px; margin-bottom: 5px;">
-                    <span style="color: white;">${cliente.nome}</span>
-                    <span style="color: white; margin-left: 10px;">Número: ${index + 1}</span>
-                </div>`;
-    }).join('');
-    document.getElementById("cliente").innerHTML = filaDisplay
+function showClient() {
+  let filaDisplay = fila
+    .map((cliente, index) => {
+      return `<div style="display: flex; align-items: center; backdrop-filter: blur(4px); padding: 10px; margin-bottom: 5px; border-radius: 10px">
+        <div style="display: flex; align-items: center;">
+            <span style="color: white;">${cliente.nome}</span>
+        </div>
+        <p>--</p>
+        <div style="width: 20px; height: 20px; border-radius: 50%; background-color: ${cliente.cor}; margin-right: 10px;">
+        <span style="color: white; margin-left: 1px;">${index + 1}</span></div>
+    </div>`;
+    })
+    .join("");
+  document.getElementById("cliente").innerHTML = filaDisplay;
 }
